0fbb2058da5526ff84aa481647d86ddf8329a2a6
tom-nachum <tom.nachum@gmail.com>
2022-09-24 11:30:23 +0300

    added title

--- server.py
+++ server.py
@@ -58,4 +58,4 @@ def get_years():
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8042, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8043, reload=True)
